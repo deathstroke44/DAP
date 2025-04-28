cf1890f5324a5c1b1b88f2bae56eda5ead23bfbe
samin_yeaser <samin.yeaser@tigerit.com>
2025-04-27 22:06:12 -0400

    added scripts

--- milvus_scripts_2/create_indexes.py
+++ milvus_scripts_2/create_indexes.py
@@ -3,7 +3,7 @@ from pymilvus import MilvusClient
 # Prepare index building params
 
 
-def create_vector_database(db_name,_collection_name,_index_type,_index_name,_metric_type,_params):
+def create_vector_database(db_name,_index_type,_index_name,_metric_type,_params):
     client = MilvusClient(db_name)
     index_params = client.prepare_index_params()
 
@@ -14,14 +14,14 @@ def create_vector_database(db_name,_collection_name,_index_type,_index_name,_met
         metric_type=_metric_type, # Metric type used to measure similarity
         params=_params
     )
-    if client.has_collection(collection_name=_collection_name):
-        client.drop_collection(collection_name=_collection_name)
+    if client.has_collection(collection_name='collection_1'):
+        client.drop_collection(collection_name='collection_1')
     client.create_collection(
-        collection_name=_collection_name
+        collection_name='collection_1'
     )
 
     client.create_index(
-        collection_name=_collection_name,
+        collection_name='collection_1',
         index_params=index_params
     )
     
